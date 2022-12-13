916953eee6470612221d7108ba632bda326bc219
winth03 <66255462+winth03@users.noreply.github.com>
2022-12-13 18:33:29 +0700

    please work

--- server/api/dungeon.js
+++ server/api/dungeon.js
@@ -1,4 +1,4 @@
-import { exec, spawn } from 'child_process'
+import { exec, execFile, spawn } from 'child_process'
 
 function myPromise(timeout, callback) {
     return new Promise((resolve, reject) => {
@@ -36,8 +36,13 @@ export default defineEventHandler(async (event) => {
 
     if (event.node.req.method === 'GET') {
         child?.kill()
-        if (useRuntimeConfig().nodeEnv !== 'development') exec('chmod a+x ./megadungeon.exe', { cwd: './public' })
-        child = spawn(useRuntimeConfig().nodeEnv === 'development' ? 'megadungeon.exe' : './megadungeon.exe', { cwd: './public', })
+        if (useRuntimeConfig().nodeEnv !== 'development') {
+            exec('chmod a+x ./megadungeon.exe', { cwd: './public' })
+            child = execFile('./megadungeon.exe', { cwd: './public', })
+        }
+        else {
+            child = spawn('megadungeon.exe', { cwd: './public', })
+        }
         console.log("spawned", child.pid)
         child.stdout.setEncoding('utf-8')
         const data = await waitForOutput()        
