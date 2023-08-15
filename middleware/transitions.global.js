2a1ada7fce81d92a6eb4c0b065e20e7c7e9aeaf1
Nutpapop Yasawut <66255462+winth03@users.noreply.github.com>
2023-08-16 01:00:47 +0700

    fix: scroll

--- middleware/transitions.global.js
+++ middleware/transitions.global.js
@@ -6,16 +6,19 @@ export default defineNuxtRouteMiddleware((to, from) => {
         'archive': 3,
     }
 
-    if (pageIndex[from.name] < pageIndex[to.name]) {
-        from.meta.pageTransition = { name: 'page-left' }
-        to.meta.pageTransition = { name: 'page-right' }
-    }
-    else if (pageIndex[from.name] > pageIndex[to.name]) {
-        from.meta.pageTransition = { name: 'page-right' }
-        to.meta.pageTransition = { name: 'page-left'}
-    }
-    else {
-        from.meta.pageTransition = { name: 'page-fade'}
-        to.meta.pageTransition = { name: 'page-fade'}
+    if (to.path !== from.path) {
+        if (process.client) window.scrollTo({top: 0, behavior: 'auto'});
+        if (pageIndex[from.name] < pageIndex[to.name]) {
+            from.meta.pageTransition = { name: 'page-left' }
+            to.meta.pageTransition = { name: 'page-right' }
+        }
+        else if (pageIndex[from.name] > pageIndex[to.name]) {
+            from.meta.pageTransition = { name: 'page-right' }
+            to.meta.pageTransition = { name: 'page-left'}
+        }
+        else {
+            from.meta.pageTransition = { name: 'page-fade'}
+            to.meta.pageTransition = { name: 'page-fade'}
+        }
     }
 })
