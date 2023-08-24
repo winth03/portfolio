6bcaa34fad2aa3892947e38bcc1a67ee3a5b83da
Nutpapop Yasawut <66255462+winth03@users.noreply.github.com>
2023-08-24 19:08:42 +0700

    quick autoplay fix

--- pages/tour.vue
+++ pages/tour.vue
@@ -1,7 +1,7 @@
 <template>
     <el-tabs>
         <el-tab-pane :label="item.title" class="mb-4" v-for="(item, index) in items" :key="index">
-            <el-carousel height="auto" indicator-position="outside" :interval="5000" :autoplay="false">
+            <el-carousel height="auto" indicator-position="outside" :interval="5000">
                 <el-carousel-item style="height: max-content;" v-for="(src, num) in item.src">
                     <el-image fit="scale-down" :src="src" :alt="`image${num}`" />
                 </el-carousel-item>
